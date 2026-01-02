fbc627ab07becc17a4d249ee40df7be43a967fd9
aaryandipnaik23 <daaryan2610@gmail.com>
2026-01-02 16:13:23 +0400

    Create MiddleOfLinkedList.java

---
+++ LinkedList/MiddleOfLinkedList.java
@@ -0,0 +1,35 @@
+public class MiddleOfLinkedList {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    static Node head;
+
+    static void findMiddle(){
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        System.out.println("Middle: " + slow.data);
+    }
+
+    public static void main(String[] args){
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        findMiddle();
+    }
+}
