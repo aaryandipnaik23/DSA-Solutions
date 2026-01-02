bfc91df408968d49bfb661a91cf8e55687d22e67
aaryandipnaik23 <daaryan2610@gmail.com>
2026-01-02 16:16:37 +0400

    Create QueueLinkedList.java

---
+++ Queue/QueueLinkedList.java
@@ -0,0 +1,50 @@
+public class QueueLinkedList {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    static Node front = null, rear = null;
+
+    static void enqueue(int x){
+        Node newNode = new Node(x);
+        if(rear == null){
+            front = rear = newNode;
+            return;
+        }
+        rear.next = newNode;
+        rear = newNode;
+    }
+
+    static void dequeue(){
+        if(front == null){
+            System.out.println("Queue Empty");
+            return;
+        }
+        front = front.next;
+        if(front == null)
+            rear = null;
+    }
+
+    static void display(){
+        Node temp = front;
+        while(temp != null){
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        enqueue(10);
+        enqueue(20);
+        enqueue(30);
+        dequeue();
+        display();
+    }
+}
