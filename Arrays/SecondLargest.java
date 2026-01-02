d295992f5d5c931247e30b23463f0ed64d4188af
aaryandipnaik23 <daaryan2610@gmail.com>
2026-01-02 16:01:55 +0400

    Create SecondLargest.java

---
+++ Arrays/SecondLargest.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class SecondLargest {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int largest = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for(int x : arr){
+            if(x > largest){
+                second = largest;
+                largest = x;
+            } else if(x > second && x != largest){
+                second = x;
+            }
+        }
+
+        if(second == Integer.MIN_VALUE)
+            System.out.println("No second largest");
+        else
+            System.out.println(second);
+        
+        sc.close();
+    }
+}
